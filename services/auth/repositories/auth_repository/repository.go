404cd37e6127b498140f388191639cff65f07a45
FFH <sevafad@gmail.com>
2026-06-06 14:11:16 +0300

    Prac-1. Work on auth service.

---
+++ services/auth/repositories/auth_repository/repository.go
@@ -0,0 +1,39 @@
+package auth_repository
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/FFH255/mag-1-2-tip/services/auth/models"
+)
+
+type Repository struct {
+	users map[string]models.User
+}
+
+func New() Repository {
+	return Repository{
+		users: make(map[string]models.User),
+	}
+}
+
+func (r *Repository) Exists(ctx context.Context, login string) (bool, error) {
+	_, ok := r.users[login]
+
+	return ok, nil
+}
+
+func (r *Repository) GetByLogin(ctx context.Context, login string) (*models.User, error) {
+	user, exists := r.users[login]
+	if !exists {
+		return nil, fmt.Errorf("user with login %s not found", login)
+	}
+
+	return &user, nil
+}
+
+func (r *Repository) Save(ctx context.Context, user *models.User) error {
+	r.users[user.Login] = *user
+
+	return nil
+}
---
+++ services/auth/services/auth_service/service.go
@@ -0,0 +1,83 @@
+package auth_service
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/FFH255/mag-1-2-tip/services/auth/models"
+	"github.com/FFH255/mag-1-2-tip/shared/jwt"
+)
+
+var (
+	UserNotFoundError      = fmt.Errorf("user not found")
+	IncorrectPasswordError = fmt.Errorf("incorrect password")
+	UserAlreadyExistsError = fmt.Errorf("user already exists")
+)
+
+type Service struct {
+	secret         string
+	expireDuration time.Duration
+	userRepository userRepository
+}
+
+type userRepository interface {
+	Exists(ctx context.Context, login string) (bool, error)
+	GetByLogin(ctx context.Context, login string) (*models.User, error)
+	Save(ctx context.Context, user *models.User) error
+}
+
+func New(secret string, expirationDuration time.Duration, userRepository userRepository) Service {
+	return Service{
+		userRepository: userRepository,
+		secret:         secret,
+		expireDuration: expirationDuration,
+	}
+}
+
+func (s Service) Register(ctx context.Context, login, password string) error {
+	exists, err := s.userRepository.Exists(ctx, login)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return UserAlreadyExistsError
+	}
+
+	user := models.NewUser(login, password)
+	if err := s.userRepository.Save(ctx, user); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s Service) Login(ctx context.Context, login, password string) (models.Token, error) {
+	user, err := s.userRepository.GetByLogin(ctx, login)
+	if err != nil {
+		return "", err
+	}
+	if user == nil {
+		return "", UserNotFoundError
+	}
+
+	if user.Password != password {
+		return "", IncorrectPasswordError
+	}
+
+	token, err := jwt.Create[models.Payload](s.secret, *models.NewPayload(login), s.expireDuration)
+	if err != nil {
+		return "", err
+	}
+
+	return models.Token(token), nil
+}
+
+func (s Service) Verify(ctx context.Context, token models.Token) error {
+	_, err := jwt.Validate[models.Payload](string(token), s.secret)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
